ce0e8b77905f5b5cde2c1e01d50047a70384b190
EmreKaratas64 <emrekaratas076@gmail.com>
2023-07-23 23:59:01 +0300

    JWT implemented, tests for jwt is done with PostMan app

---
+++ JWT/JWTAPI/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using JWTAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [HttpGet("[action]")]
+        public IActionResult GetJWT()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult TestJWT()
+        {
+            return Ok("Hoşgeldiniz");
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetAdminJWT()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult TestAdminJWT()
+        {
+            return Ok("Admin token ile giriş yapıldı");
+        }
+
+
+    }
+}
