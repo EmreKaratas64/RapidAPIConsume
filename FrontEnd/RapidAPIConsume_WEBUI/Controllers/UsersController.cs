4ca526f063c32fe84d22dfebb3fbf59ed9a4c11b
EmreKaratas64 <emrekaratas076@gmail.com>
2023-08-02 22:28:33 +0300

    user rolling finished

--- FrontEnd/RapidAPIConsume_WEBUI/Controllers/UsersController.cs
+++ FrontEnd/RapidAPIConsume_WEBUI/Controllers/UsersController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RapidAPIConsume_EntityLayer.Concrete;
 using RapidAPIConsume_WEBUI.DTOs.AppUserDtos;
+using RapidAPIConsume_WEBUI.DTOs.RoleDtos;
 
 namespace RapidAPIConsume_WEBUI.Controllers
 {
     public class UsersController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
 
-        public UsersController(IHttpClientFactory httpClientFactory)
+        public UsersController(IHttpClientFactory httpClientFactory, UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             _httpClientFactory = httpClientFactory;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task<IActionResult> ListUsers()
@@ -25,5 +32,41 @@ namespace RapidAPIConsume_WEBUI.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int Id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == Id);
+            TempData["UserId"] = Id;
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignDto> roleAssignDto = new List<RoleAssignDto>();
+            foreach (var role in roles)
+            {
+                RoleAssignDto model = new RoleAssignDto();
+                model.RoleId = role.Id;
+                model.RoleName = role.Name;
+                model.RoleExists = userRoles.Contains(role.Name);
+                roleAssignDto.Add(model);
+            }
+            return View(roleAssignDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignDto> roleAssignDto)
+        {
+            var userId = (int)TempData["UserId"];
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userId);
+            foreach (var item in roleAssignDto)
+            {
+                if (item.RoleExists)
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                else
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+            }
+
+            return RedirectToAction("ListUsers");
+        }
+
     }
 }
---
+++ FrontEnd/RapidAPIConsume_WEBUI/DTOs/RoleDtos/RoleAssignDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RapidAPIConsume_WEBUI.DTOs.RoleDtos
+{
+    public class RoleAssignDto
+    {
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public bool RoleExists { get; set; }
+    }
+}
